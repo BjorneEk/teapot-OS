741c0fdeee753d35e2e34ff022a1899550ccc00f
Bjorneek <gustaffranzen@icloud.com>
2022-01-25 14:07:43 +0100

    implemented function sin(x)

--- src/kernel/utils/lib/math.c
+++ src/kernel/utils/lib/math.c
@@ -5,3 +5,45 @@
 
 
 #include "../include/math.h"
+
+
+float pow(float x, uint32_t p) {
+	for (size_t i = 0; i < p; i++) {
+		x*=x;
+	}
+	return x;
+}
+
+float mod(float a, float b) {
+	double mod= (a < 0) ? -a : a;
+	if (b < 0) b = -b;
+	// finding mod by repeated subtraction
+	while (mod >= b) mod -= b;
+	return (a < 0) ? -mod : mod;
+}
+
+/**
+ *  Sine function based on taylor series of sin(x)
+ *
+ *  the function __taylor_sin is fairly accurate between -PI/3 and PI;
+ *
+ *  the sine function uses further logic to extend this range for ex since sin(-x) == -sin(x)
+ *  the accuracy can be improved to -PI to PI;
+ *
+ * see docs/sin.txt for comparison with math.h sin(x); and graphical comparison
+ * between __taylor_sin(x) and sin(x)
+ **/
+
+static inline __attribute__((always_inline)) float __taylor_sin(float x){
+	return (1.00003 * x) - (0.000312267 * pow(x, 2)) -
+		(0.165537 * pow(x, 3)) - (0.00203937 * pow(x, 4)) +
+			(0.010286 * pow(x, 5)) - (0.000961693 * pow(x, 6));
+}
+
+float sin(float x){
+	uint8_t sign;
+	float _x = (sign = (x < 0)) ? -x : x;
+	sign = ((!(((uint32_t)(x / (float)(PI))) % 2)) == sign);
+	_x = mod(_x, PI);
+	return (sign) ? -__taylor_sin(_x) : __taylor_sin(_x);
+}
