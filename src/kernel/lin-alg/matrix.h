cf212b3c933fc65c5c51c47eb6de508550a9643f
Bjorneek <gustaffranzen@icloud.com>
2022-01-28 00:57:15 +0100

    extended linear algebra library

---
+++ src/kernel/lin-alg/matrix.h
@@ -0,0 +1,56 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+////////////////////////////////////////////////////////////////////////////
+///        @author Gustaf Franzén :: https://github.com/BjorneEk;        ///
+////////////////////////////////////////////////////////////////////////////
+
+#include "../libc/include/int.h"
+#include "vector.h"
+
+typedef struct matrix3x3 {
+	union {
+		struct {
+			vec3d_t r1;
+			vec3d_t r2;
+			vec3d_t r3;
+		};
+		float m[3][3];
+	};
+} matrix3x3_t;
+
+typedef struct matrix4x4 {
+	float m[4][4];
+} matrix4x4_t;
+
+
+
+matrix3x3_t rot_x_mat3x3(float v);
+matrix3x3_t rot_y_mat3x3(float v);
+matrix3x3_t rot_z_mat3x3(float v);
+
+matrix4x4_t rot_x_mat4x4(float v);
+matrix4x4_t rot_y_mat4x4(float v);
+matrix4x4_t rot_z_mat4x4(float v);
+
+matrix4x4_t translation_matrix(vec3d_t pos);
+matrix4x4_t scaling_matrix(vec3d_t xyz_scale);
+
+matrix4x4_t projection_matrix(float near, float far, float fov, float ar);
+
+void mat4x4_to_str(matrix4x4_t m, char str[]);
+
+vec3d_t project_vec(vec3d_t v, matrix4x4_t m);
+
+
+/**
+ *  not true v3d * m4x4 multiplication
+ *  but special case used for projection
+ **/
+vec3d_t mat4x4_vec_mult(vec3d_t v, matrix4x4_t m);
+vec3d_t mat3x3_vec_mult(vec3d_t v, matrix3x3_t m);
+
+matrix4x4_t mat4x4_mult(matrix4x4_t m1, matrix4x4_t m2);
+matrix3x3_t mat3x3_mult(matrix3x3_t m1, matrix3x3_t m2);
+
+#endif /* MATRIX_H */
---
+++ src/kernel/lin-alg/triangle.h
@@ -0,0 +1,51 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+////////////////////////////////////////////////////////////////////////////
+///        @author Gustaf Franzén :: https://github.com/BjorneEk;        ///
+////////////////////////////////////////////////////////////////////////////
+
+#include "../libc/include/int.h"
+#include "../graphics/graphics.h"
+#include "matrix.h"
+
+
+typedef struct triangle3d {
+	union {
+		struct {
+			vec3d_t p1;
+			vec3d_t p2;
+			vec3d_t p3;
+			vec3d_t normal;
+		};
+		vec3d_t p[4];
+	};
+} triangle3d_t;
+
+/**
+ *   average Z value in all point of trianlge
+ *   this function is used to sort a mesh before
+ *   it is drawn to the screen.
+ **/
+float z_avg(triangle3d_t * tri);
+
+uint8_t tri_contains(triangle3d_t tri, vec3d_t p);
+
+uint8_t equals_tri(triangle3d_t t1, triangle3d_t t2);
+
+void tri_set_normal(triangle3d_t * tri);
+
+triangle3d_t triangle_mult_matrix(triangle3d_t tri, matrix4x4_t m);
+
+triangle3d_t rotate_tri(triangle3d_t tri, float x, float y, float z);
+
+triangle3d_t translate_tri(triangle3d_t tri, vec3d_t pos);
+
+triangle3d_t scale_tri(triangle3d_t tri, float scale);
+
+triangle3d_t project_tri(triangle3d_t tri, float near, float far, float fov, float ar);
+
+void draw_triangle(triangle3d_t tri, color_t clr);
+
+
+#endif /* triangle_H */
